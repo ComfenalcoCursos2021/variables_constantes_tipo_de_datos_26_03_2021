fc7813ac0620ca0fac2afe44ec48290e031faae0
Miguel Angel Castro Escamilla <ma98030965140@gmail.com>
2021-03-26 20:29:39 -0500

    26-03-2021

---
+++ main.js
@@ -0,0 +1,42 @@
+
+
+
+
+nombre = "Miguel";
+console.log(nombre);
+
+
+
+
+
+// Variables y constantes
+
+// let nombre;
+// let apellido;
+// const edad = 23;
+// nombre = "Miguel";
+// apellido = "Castro";
+// console.log(nombre, apellido, edad);
+
+
+// Tipos de datos
+let nombre = `Miguel Angel Castro Escamilla`; //String
+let edad = 23; //number - intenger - int;
+let altura = 1.63; //Double - float
+let profesor = true; // Boleano
+let informacion = [nombre, edad, altura, profesor]; //Array
+// console.log(informacion);
+// console.log(informacion[2]);
+let baso = {
+	material : "porcelan",
+	diametro :  60,
+	lleno : false
+}; // Object
+// console.log(baso);
+// console.log(baso.diametro);
+
+
+
+
+
+
